6180a4f0ccc4957ce6b86072b58d91db14e6c9bb
Felipe Berlini <felipe.berlini@gmail.com>
2018-03-19 21:22:52 -0300

    Reconhecimento de imagem
    Implementação da chamada do serviço de reconhecimento facial do Azure

--- GeekBurger.Users/GeekBurger.Users/Entities/FacePicture.cs
+++ GeekBurger.Users/GeekBurger.Users/Entities/FacePicture.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GeekBurger.Users.Entities
 {
-    public class Face
+    public class FacePicture
     {
+        public Guid FaceId { get; set; }
+        public Stream ImageStream { get; set; }
     }
 }
--- GeekBurger.Users/GeekBurger.Users/Services/FaceDetectionService.cs
+++ GeekBurger.Users/GeekBurger.Users/Services/FaceDetectionService.cs
@@ -1,27 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GeekBurger.Users.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.ProjectOxford.Face;
 
 namespace GeekBurger.Users.Services
 {
     public class FaceDetectionService : IFaceDetection
     {
-        public Task<Face> Detect(Stream picture)
+        public static FaceServiceClient faceServiceClient;
+        public static IConfiguration Configuration;
+        public static Guid FaceListId;
+
+        public FaceDetectionService(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            Configuration = configuration;
+
+            FaceListId = Guid.Parse(Configuration["FaceListId"]);
+
+            faceServiceClient = new FaceServiceClient(Configuration["FaceAPIKey"], "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/");
+
+            UpsertFaceListAndCheckIfContainsFace();
         }
 
-        public Task<(Guid userId, double Confidence)> FindSimilars(Face face)
+        public async Task<FacePicture> Detect(Stream picture)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var faces = await faceServiceClient.DetectAsync(picture);
+                var faceReturned = faces.FirstOrDefault();
+
+                if (faceReturned != null)
+                {
+                    FacePicture face = new FacePicture
+                    {
+                        FaceId = faceReturned.FaceId,
+                        ImageStream = picture
+                    };
+
+                    return face;
+                }
+
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
-        public Task Save(Guid userID, Face face)
+        public async Task<(Guid userId, double Confidence)?> FindSimilars(FacePicture face)
         {
-            throw new NotImplementedException();
+            var similarFaces = await faceServiceClient.FindSimilarAsync(face.FaceId, FaceListId.ToString(), 5);
+
+            var similarFace = similarFaces.FirstOrDefault(_ => _.Confidence > 0.5);
+
+            return (similarFace.PersistedFaceId, similarFace.Confidence);
+        }
+
+        public async Task Save(Guid userID, FacePicture face)
+        {
+            try
+            {
+                await faceServiceClient.AddFaceToFaceListAsync(FaceListId.ToString(), face.ImageStream);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Face not included in Face List!");
+            }
+        }
+
+        private void UpsertFaceListAndCheckIfContainsFace()
+        {
+            var faceListId = FaceListId.ToString();
+            var faceLists = faceServiceClient.ListFaceListsAsync().Result;
+            var faceList = faceLists.FirstOrDefault(_ => _.FaceListId == FaceListId.ToString());
+
+            if (faceList == null)
+            {
+                faceServiceClient.CreateFaceListAsync(faceListId, "GeekBurgerFaces", null);
+            }
         }
     }
 }
